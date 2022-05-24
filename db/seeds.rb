9ff0863d383b34bf74d804b19bebe1017d5afc64
Igor Martins <igordavillamartins@outlook.com>
2022-05-24 16:59:53 -0300

    created 10 articles with faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do |i|
+    post = Article.new(
+      title: "My article number #{i}",
+      content: Faker::Lorem.paragraph(sentence_count: 2)
+    )
+    post.save
+  end
